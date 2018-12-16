425652665ebe17d73e12351c61df47ffb1ba428a
Daniel Costa <daniel.costa@sayduck.com>
2018-12-16 22:31:13 +0100

    + MainStage + Light

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from 'react';
+import MainStage from '../MainStage';
+import Light from '../Elements/Light';
 
 class App extends Component {
+  state = {
+    elements: [],
+  };
+
+  addShape = () => {
+    this.setState(prevState => ({
+      elements: [...prevState.elements, <Light x={200} y={100} />],
+    }));
+  };
+
   render() {
-    return <div>Test</div>;
+    const { elements } = this.state;
+    return (
+      <Fragment>
+        <MainStage elements={elements} />
+        <button type="button" onClick={this.addShape}>
+          Add element
+        </button>
+      </Fragment>
+    );
   }
 }
 
